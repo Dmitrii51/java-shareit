620d25d70c66efc74d39c0f54554e4841c961231
MaminChampion <pyrkov51@yamdex.ru>
2022-08-30 22:29:43 +0300

    add TODO

--- src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/requests/ItemRequestController.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.requests;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//TODO Realize ItemRequestController in in 15-th sprint
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
--- src/main/java/ru/practicum/shareit/requests/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/requests/dto/ItemRequestDto.java
@@ -1,4 +1,5 @@
 package ru.practicum.shareit.requests.dto;
 
+//TODO Realize ItemRequestDto in 15-th sprint
 public class ItemRequestDto {
 }
